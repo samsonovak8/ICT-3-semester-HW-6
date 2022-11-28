30eb320b6dd305c39ca2e849e758834bc15deb3a
Katya Samsonova <89536093+samsonovak8@users.noreply.github.com>
2022-11-29 00:03:10 +0300

    убрала всю глобальщину!!

--- e.cpp
+++ e.cpp
@@ -3,59 +3,63 @@
 #include<string>
 #include<queue>
 
-std::vector<std::vector<bool> > used(2005, std::vector<bool>(2005, 0));
-std::vector<std::string> s(2005);
+struct Graph {
+    std::vector<std::vector<bool> > used = std::vector<std::vector<bool> >(2005, std::vector<bool>(2005, 0));
+    std::vector<std::string> s = std::vector<std::string>(2005);
 
-void dfs(int x, int y, size_t n, size_t m) {
+    void dfs(int x, int y, size_t n, size_t m) {
 
-    std::queue<int> q;
-    q.push(x);
-    q.push(y);
-    used[x][y] = 1;
-    while(!q.empty()) {
-        int u = q.front();
-        q.pop();
-        int v = q.front();
-        q.pop();
-        if (u && !used[u - 1][v] && s[u - 1][v] == '+') {
-            q.push(u - 1);
-            q.push(v);
-            used[u - 1][v] = 1;
+        std::queue<int> q;
+        q.push(x);
+        q.push(y);
+        used[x][y] = 1;
+        while(!q.empty()) {
+            int u = q.front();
+            q.pop();
+            int v = q.front();
+            q.pop();
+            if (u && !used[u - 1][v] && s[u - 1][v] == '+') {
+                q.push(u - 1);
+                q.push(v);
+                used[u - 1][v] = 1;
+            }
+            if(v && !used[u][v - 1] && s[u][v - 1] == '+') {
+                q.push(u);
+                q.push(v - 1);
+                used[u][v - 1] = 1;
+            }
+            if (u + 1 < n && !used[u + 1][v] && s[u + 1][v] == '+') {
+                q.push(u + 1);
+                q.push(v);
+                used[u + 1][v] = 1;
+            }
+            if (v + 1 < m && !used[u][v + 1] && s[u][v + 1] == '+') {
+                q.push(u);
+                q.push(v + 1);
+                used[u][v + 1] = 1;
+            }
         }
-        if(v && !used[u][v - 1] && s[u][v - 1] == '+') {
-            q.push(u);
-            q.push(v - 1);
-            used[u][v - 1] = 1;
-        }
-        if (u + 1 < n && !used[u + 1][v] && s[u + 1][v] == '+') {
-            q.push(u + 1);
-            q.push(v);
-            used[u + 1][v] = 1;
-        }
-        if (v + 1 < m && !used[u][v + 1] && s[u][v + 1] == '+') {
-            q.push(u);
-            q.push(v + 1);
-            used[u][v + 1] = 1;
-        }
+        return;
     }
-    return;
-}
+};
 
 int main() {
 
     size_t n, m;
     std::cin >> n >> m;
 
+    Graph graph;
+
     for(size_t i = 0; i < n; ++i) {
-        std::cin >> s[i];
+        std::cin >> graph.s[i];
     }
 
     int cnt = 0;
     for(int i = 0; i < n; ++i) {
         for(int j = 0; j < m; ++j) {
-            if (s[i][j] == '+' && !used[i][j]) {
+            if (graph.s[i][j] == '+' && !graph.used[i][j]) {
                 ++cnt;
-                dfs(i, j, n, m);
+                graph.dfs(i, j, n, m);
             }
         }
     }   
